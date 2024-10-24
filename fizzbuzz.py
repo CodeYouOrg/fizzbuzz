787a45ed47f4d691ba65fcb104065a2e3ee34f85
sampss <sampsons79@gmail.com>
2024-10-24 17:55:32 -0400

    added fizzbuzz code, mine allows input for range so it is unlikely to work with test code.  it also allows to increment by selected amounts.  I have tested it and it works.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,28 @@
-# add your code here
+
+def fizzbuzz(start, end, increment) -> str:
+    output = ''
+    for number in range(start, end, increment):
+        if number % 3 == 0 and number % 5 == 0:
+            output += 'FizzBuzz\n'
+        elif number % 3 == 0:
+            output += 'Fizz\n'
+        elif number % 5 == 0:
+            output += 'Buzz\n'
+        else:
+            output += f'{number}\n'
+
+    return output
+
+def main():
+    start = int(input('Where to start? '))  # Convert input to int
+    end = int(input('Where to end? '))      # Convert input to int
+    increment = int(input('What should we count by? '))  # Convert input to
+
+    results = fizzbuzz(start, end, increment)
+
+    print(results)
+
+if __name__ == "__main__":
+    main()
+
 
