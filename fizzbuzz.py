f2ecedbe7652490477b5e6560f496b540b2fc26b
Carolina Bailey <cmonjarr@gmail.com>
2024-10-27 21:30:39 -0500

    Updated fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,7 @@
 # add your code here
-
+fizzbuzz = ["FizzBuzz" if i % 15 == 0
+            else "Fizz" if i % 3 == 0
+            else "Buzz" if i % 5 == 0 
+            else i for i in range(1,101)]
+for i in fizzbuzz:
+    print (i)
