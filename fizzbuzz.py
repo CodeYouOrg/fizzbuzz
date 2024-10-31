76bfbcdf6334506df01b104edcc4717be57f7221
Redsoxin7 <Redsoxin7+code@pm.me>
2024-10-30 23:10:28 -0400

    First commit for FizzBuzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 # add your code here
-
+numb = 0
+while numb <= 99:
+    numb = numb + 1
+    if numb % 3 == 0 and numb % 5 == 0:
+        print("FizzBuzz")
+    elif numb % 3 == 0:
+        print("Fizz")
+    elif numb % 5 == 0:
+        print("Buzz")
+    else:
+        print(numb)
