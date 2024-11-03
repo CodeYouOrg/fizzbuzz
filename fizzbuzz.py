8d5755ca4d42050dfb7982bc9dba4a030ad2978e
MalaikaMaisha <malaikacatching@gmail.com>
2024-11-03 15:35:35 -0500

    Updated

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-# add your code here
-
+# Print numbers 1-100 with "FizzBuzz" for multiples of 3 and 5, "Fizz" for multiples of 3, and "Buzz" for multiples of 5
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
