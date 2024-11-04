c284cd1e81dc06a7ad6f8942e26dde986684971f
ira-onic <ira.eliott.h@gmail.com>
2024-11-03 22:19:33 -0500

    FizzBuzz Program

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
-# add your code here
+current_value = 1
+while current_value <= 100:
 
+    if current_value % 3 ==0 and current_value % 5 == 0:
+        print ("FizzBuzz")
+    elif current_value % 3 == 0:
+        print ("Fizz")
+    elif current_value % 5 == 0:
+        print ("Buzz")
+    else:
+        print (current_value)
+
+    current_value += 1
